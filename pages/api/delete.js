35362e188a784ee688ffbbdef6215d1a0ed696cb
NabNabery <paulosouzadev@outlook.com>
2022-06-30 21:35:54 -0300

    test

--- pages/api/delete.js
+++ pages/api/delete.js
@@ -1,7 +1,5 @@
-async function deletecall (request,response) {
-    const { body } = request;
-
-    response.send(body.name)
-}
-
-export default deletes
+export default async function handler(req, res) {
+    const { body } = req;
+    return res.send(`Hello ${body.name}, you just parsed the request body!`);
+  }
+  

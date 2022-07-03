187db6ab9baeb964219ff6300a3929046523e21a
NabNabery <paulosouzadev@outlook.com>
2022-07-03 09:09:26 -0300

    'tesataer'

--- pages/api/get.js
+++ pages/api/get.js
@@ -2,9 +2,8 @@ import { response } from "express";
 import BoardData from "/data/board-data.json";
 
 export default async function handler(req, res) {
-    let obj = BoardData
 
-    res.setHeader('Cache-Control', 's-maxage=01', 'stale-while-revalidate = true');
+    response.setHeader('Cache-Control', 's-maxage=01', 'stale-while-revalidate = true');
 
     return res.send(BoardData);
   }

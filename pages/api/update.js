6c49e5f0cb68206396e62221075fd22243debf36
NabNabery <paulosouzadev@outlook.com>
2022-07-04 14:35:49 -0300

    test

--- pages/api/update.js
+++ pages/api/update.js
@@ -5,10 +5,11 @@ import path from 'path';
 
 export default function handler(req, res) {
   let obj = req.body;
+  let data = JSON.stringify(obj)
   const file = path.join(process.cwd(), 'data', 'board-data.json');
   const stringified = readFileSync(file, 'utf8');
-  
-  fs.writeFileSync(file, obj)
+
+  fs.writeFileSync(file, data)
   res.setHeader('Content-Type', 'application/json');
   return res.end(stringified);
 }

5743d1cb991dca7b6f61cd312c604062f5c9f2d1
NabNabery <paulosouzadev@outlook.com>
2022-07-04 13:10:52 -0300

    dasd

--- pages/api/update.js
+++ pages/api/update.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 
 export default function updates(req,res) {
   let obj = req.body
-  fs.writeFileSync('/data/board-data.json', JSON.stringify(obj))
+  fs.writeFileSync('../data/board-data.json', JSON.stringify(obj))
   res.send(obj)
 }

6b2974da7b2bc67e85b002b0bca03238a34784c2
NabNabery <paulosouzadev@outlook.com>
2022-07-04 13:56:52 -0300

    123123asdmasd

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,11 +1,18 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+const fs = require('fs')
+
+export default function updates(req,res) {
+  let obj = req.body
+  fs.writeFileSync('./data/board-data.json', JSON.stringify(obj))
+  res.send(obj)
+}
+
 
 export default async function handler(req, res) {
+  let obj = req.body
   //Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'data');
   //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/board-data.json', 'utf8');
+  fs.writeFileSync(jsonDirectory + '/board-data.json', JSON.stringify(obj))
   //Return the content of the data file in json format
-  res.status(200).json(fileContents);
+  res.status(200).json(obj);
 }

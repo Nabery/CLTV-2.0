f1b569642fc45e2eb7af9704eabf160a2ecb69d2
NabNabery <paulosouzadev@outlook.com>
2022-07-04 14:10:09 -0300

    amsidaismd

--- pages/api/update.js
+++ pages/api/update.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   //Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'data');
   //Read the json data file data.json
-  fs.writeFileSync(jsonDirectory + '/board-data.json', JSON.stringify(obj))
+  const test = fs.writeFileSync(jsonDirectory + '/board-data.json', JSON.stringify(obj))
   //Return the content of the data file in json format
-  res.status(200).json(obj);
+  res.status(200).json(test);
 }

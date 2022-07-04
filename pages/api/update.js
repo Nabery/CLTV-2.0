54574aac946fe3184bc2744c02e49b959dbcce6a
NabNabery <paulosouzadev@outlook.com>
2022-07-04 13:43:26 -0300

    adsasijdasijeiasjdnia

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-
-let data = fs.readFileSync('./data/board-data.json', {flag:'r'} )
-let parsed = JSON.parse(data.toString())
+import boardData from './data/board-data.json'
 
 export default function updates(req,res) {
   let obj = req.body
-  fs.writeFileSync('./data/board-data.json', JSON.stringify(obj))
+  //fs.writeFileSync('./data/board-data.json', JSON.stringify(obj))
+  console.log(boardData)
   res.send(obj)
+
 }

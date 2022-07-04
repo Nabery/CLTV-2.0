0382110aa03cfc3ee539b9b0e18b941a4f9391e3
NabNabery <paulosouzadev@outlook.com>
2022-07-04 12:55:00 -0300

    testsadasd

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 
-let data = fs.readFileSync('./data/board-data.json', {flag:'r'} )
-
 export default function updates(req,res) {
   let obj = req.body
   fs.writeFileSync('/data/board-data.json', JSON.stringify(obj))

da316760220b0d4c7fab56a28ddbf6bfd0f04879
NabNabery <paulosouzadev@outlook.com>
2022-07-04 14:38:53 -0300

    asuhdauhds

--- pages/api/update.js
+++ pages/api/update.js
@@ -4,12 +4,12 @@ import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  let obj = req.body;
-  let data = JSON.stringify(obj)
+
   const file = path.join(process.cwd(), 'data', 'board-data.json');
   const stringified = readFileSync(file, 'utf8');
 
-  fs.writeFileSync(file, data)
+  fs.writeFileSync(file, "aushdahusdhu")
+  
   res.setHeader('Content-Type', 'application/json');
   return res.end(stringified);
 }

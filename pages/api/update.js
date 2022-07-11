3267112a51f62a57fdebe77be7c9700cffc31860
NabNabery <paulosouzadev@outlook.com>
2022-07-11 12:08:38 -0300

    uhasdhuasd

--- pages/api/update.js
+++ pages/api/update.js
@@ -2,21 +2,22 @@ const fs = require('fs');
 const axios = require('axios')
 
 export default function createJson (req, res){
-    let data = req.body
-	const config  = {
-			method: 'POST',
-			url: 'https://getpantry.cloud/apiv1/pantry/0e12a1da-6eeb-4bd9-985c-28217a25f86c/basket/testjson',
-			headers: { 
-			  'Content-Type': 'application/json'
-			},
-			data : {data}
-		          };
-	axios(config)
-	  .then(function (response) {
-		console.log(JSON.stringify(response.data));
-	  })
-	  .catch(function (error) {
-		console.log(error);
-	  }); 
-	res.send({"Sucess:" : {data}})
-}
+     let data = req.body
+	 const config = {
+	  method: 'post',
+	  url: 'https://getpantry.cloud/apiv1/pantry/0e12a1da-6eeb-4bd9-985c-28217a25f86c/basket/finaldatajson',
+	  headers: { 
+		'Content-Type': 'application/json'
+	  },
+	  data : {data}
+	 };
+	
+	 axios(config)
+	 .then(function (response) {
+	  console.log(JSON.stringify(response.data));
+	 })
+	 .catch(function (error) {
+	  console.log(error);
+	 });
+	 res.send({data})
+	}

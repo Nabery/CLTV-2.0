22158cbe2bdff3abf4ef9151141b074aae8ba236
NabNabery <paulosouzadev@outlook.com>
2022-07-11 11:59:06 -0300

    idk what happened

--- pages/api/update.js
+++ pages/api/update.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 
 export default function createJson (req, res){
     let data = req.body
-
 	const config  = {
 			method: 'POST',
 			url: 'https://getpantry.cloud/apiv1/pantry/0e12a1da-6eeb-4bd9-985c-28217a25f86c/basket/testjson',

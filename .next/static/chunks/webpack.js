d1ea33984feabd0f195ee5a9ac0b21e1f3c3f8bf
NabNabery <paulosouzadev@outlook.com>
2022-07-11 12:35:34 -0300

    test

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2895ef55b21ceaf1"; }
+/******/ 		__webpack_require__.h = function() { return "66e6b7c77e56714f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
